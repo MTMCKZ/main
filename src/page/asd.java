2f2835df373a01dbd3d0d43e19273b242d8f1b57
mckz <mckz@qq.com>
2019-12-26 11:21:05 +0800

    dev1

--- src/page/asd.java
+++ src/page/asd.java
@@ -2,7 +2,6 @@ package page;
 
 public class asd {
     public static void main(String[] args) {
-        System.out.println("梅川酷子");
-        System.out.println("第二次增加信息");
+        System.out.println("梅川酷子dev");
     }
 }
